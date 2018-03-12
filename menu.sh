b50fca10e3fbaec3853784f19587925194c2e180
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 12:52:02 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Menu
+
+echo -e "* menu      : menampilkan daftar perintah"
+echo -e "* usernew   : membuat akun SSH & OpenVPN"
+echo -e "* trial     : membuat akun trial"
+echo -e "* hapus     : menghapus akun SSH & OpenVPN"
+echo -e "* cek       : cek user login"
+echo -e "* member    : daftar member SSH & OpenVPN"
+echo -e "* resvis    : restart service dropbear, webmin"
+echo -e "              squid3, OpenVPN dan SSH"
+echo -e "* reboot    : reboot VPS"
+echo -e "* speedtest : speedtest VPS"
+echo -e "* info      : menampilkan informasi sistem"
+echo -e "* about     : info script auto install"
+echo -e "* exit      : keluar dari Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e ""
