04f24363eab7e7ae13c37e1cceb1a29f1d5a69ed
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 12:53:26 +0800

    Create user-login.sh

---
+++ user-login.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Script Copyright www.fornesia.com
+# Mod by Bustami Arifin
+# ==========================
+# 
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+echo "-----------------------";
+echo "Checking Dropbear login";
+echo "-----------------------";
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+
+echo "";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "----------------------";
+echo "Checking OpenSSH login";
+echo "----------------------";
+
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+	IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+
+echo "";
+
+echo "------------------------------------------------"
+echo " Multi Login = kill "
+echo " Cara pakai : kill nomor PID "
+echo "------------------------------------------------"
+
+echo "";
+
+echo "Mod by Bustami Arifin";
+
+echo "";
