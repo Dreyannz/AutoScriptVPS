d62aed83abd2922b7feebaed729165810de2e61b
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 12:55:58 +0800

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" 
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Mod by Bustami Arifin"
+echo -e ""
+echo -e "https://www.facebook.com/bustami.arifin.127"
+echo -e "http://www.kangarif.net"
+echo -e ""
