6a0053ae5dfca73c4d182c08731f5869dea70059
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 13:25:10 +0800

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -13,7 +13,6 @@ do
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "-------------------------------"
-echo "Jumlah akun: $JUMLAH user"
+echo "Number of Accounts : $JUMLAH user"
 echo "-------------------------------"
-echo -e "Mod by Bustami Arifin"
 echo -e ""
