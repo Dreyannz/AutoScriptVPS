c746b100b7b6b3df2a1091f30620a93d97d89912
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 15:50:09 +0800

    Update resvis.sh

--- resvis.sh
+++ resvis.sh
@@ -2,6 +2,18 @@
 # Script restart service dropbear, webmin, squid3, openvpn, openssh
 # Created by Bustami Arifin
 clear
+echo -e ""
+echo -e ":::::::::  :::::::::: :::::::::  :::::::::::"
+echo -e ":+:    :+: :+:        :+:    :+: :+:     :+:"
+echo -e "+:+    +:+ +:+        +:+    +:+        +:+ "
+echo -e "+#+    +:+ +#++:++#   +#++:++#+        +#+  "
+echo -e "+#+    +#+ +#+        +#+    +#+      +#+   "
+echo -e "#+#    #+# #+#        #+#    #+#     #+#    "
+echo -e "#########  ########## #########      ###    "
+echo -e ""
+echo -e "Deb7AutoScriptVPS by _Dreyannz_"
+echo -e ""
+echo -e ""
 service dropbear restart
 service webmin restart
 service squid3 restart
