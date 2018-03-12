15ca429c1bf460ad88e4ad6feb52baefabb09aab
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-12 13:35:14 +0800

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#Script auto create trial user SSH
-#yg akan expired setelah 1 hari
-#modified by Bustami Arifin
+#AutoScript Create Trial Accounts
+#All Trial Accounts last for 1 day
+#Modified by Bustami Arifin
+#Translated by _Dreyannz_
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
@@ -12,14 +13,22 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Trial SSH Account===="
-echo -e "Host: $IP" 
-echo -e "Port OpenSSH: 22,143"
-echo -e "Port Dropbear: 80,443"
-echo -e "Port Squid: 8080,3128"
-echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
-echo -e "Username: $Login"
-echo -e "Password: $Pass\n"
-echo -e "========================="
-echo -e "Mod by Bustami Arifin"
+echo -e ""
+echo -e ""
+echo -e "===Trial Account Information==="
+echo -e ""
+echo -e "Username        : $Login"
+echo -e "Password        : $Pass\n"
+echo -e ""
+echo -e "Host / IP       : $IP" 
+echo -e "Port OpenSSH    : 22, 444"
+echo -e "Port Dropbear   : 143, 3128"
+echo -e "SSL             : 443"
+echo -e "Port Squid      : 8080, 8000"
+echo -e "Port OpenVPN    : TCP 1194"
+echo -e "OpenVPN Config  : http://$IP:81/client.ovpn"
+echo -e ""
+echo -e "==============================="
+echo -e ""
+echo -e ""
 echo -e ""
