659b9bce64deee60e9e2cbed81a6e4e7a5735a24
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-03-16 11:58:07 +0800

    Update servermessage.sh

--- servermessage.sh
+++ servermessage.sh
@@ -14,35 +14,23 @@ echo -e ""
 echo -e "Deb7AutoScriptVPS by _Dreyannz_"
 echo -e ""
 echo -e ""
-read -p "Username        : " Login
-read -p "Password        : " Pass
-read -p "How Many Days?  : " masaaktif
-
-IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "READ ME FIRST"
+echo -e ""
+echo -e "* To Save Changes in Nano Editor *"
+echo -e "Press Ctrl+X,"
+echo -e "Press Y,"
+echo -e "Press Enter,"
 echo -e ""
 echo -e ""
-echo -e ""
-echo -e "======Account Information======"
-echo -e ""
-echo -e "Username        : $Login "
-echo -e "Password        : $Pass"
-echo -e "Expires on      : $exp"
-echo -e ""
-echo -e "Host / IP       : $IP" 
-echo -e "Port OpenSSH    : 22, 444"
-echo -e "Port Dropbear   : 80, 443"
-echo -e "Port SSL        : 143"
-echo -e "Port Squid      : 8000, 8080"
-echo -e "Port OpenVPN    : TCP 1194"
-echo -e "OpenVPN Config  : http://$IP:81/client.ovpn"
-echo -e ""
-echo -e "==============================="
+echo -e "* To Close Nano Editor *"
+echo -e "Press Ctrl+Z"
 echo -e ""
 echo -e ""
+echo -e "* To Change Server Message *"
+echo -e "Please input the following command:"
+echo -e "nano /etc/issue.net"
 echo -e ""
-
+echo -e ""
 
 
