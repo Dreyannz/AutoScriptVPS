e8d8b10e3f1ff3e95e1049a0656372dec4531d6f
Adrian Jann Octat <31035460+Dreyannz@users.noreply.github.com>
2018-04-04 16:52:09 +0800

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -2,23 +2,22 @@
 #Script to Remove Users on SSH and OpenVPN
 
 clear
+echo -e "                                                        "
+echo -e "\e[94m   :::::::::  :::::::::: :::::::::  :::::::::::   "
+echo -e "\e[94m   :+:    :+: :+:        :+:    :+: :+:     :+:   "
+echo -e "\e[94m   +:+    +:+ +:+        +:+    +:+        +:+    "
+echo -e "\e[94m   #+    +:+ +#++:++#   +#++:++#+        +#+      "
+echo -e "\e[94m   +#+    +#+ +#+        +#+    +#+      +#+      "
+echo -e "\e[94m   #+#    #+# #+#        #+#    #+#     #+#       "
+echo -e "\e[94m   #########  ########## #########      ###       "
+echo -e "\e[94m         Deb7AutoScriptVPS by _Dreyannz_          "
+echo -e "\e[0m                                                   "
 echo -e ""
-echo -e ":::::::::  :::::::::: :::::::::  :::::::::::"
-echo -e ":+:    :+: :+:        :+:    :+: :+:     :+:"
-echo -e "+:+    +:+ +:+        +:+    +:+        +:+ "
-echo -e "+#+    +:+ +#++:++#   +#++:++#+        +#+  "
-echo -e "+#+    +#+ +#+        +#+    +#+      +#+   "
-echo -e "#+#    #+# #+#        #+#    #+#     #+#    "
-echo -e "#########  ########## #########      ###    "
-echo -e ""
-echo -e "Deb7AutoScriptVPS by _Dreyannz_"
-echo -e ""
-echo -e ""
-read -p "Username to be removed : " Pengguna
+read -p "        Username to be removed : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna
-        echo -e "User $Pengguna has been deleted successfully."
+        echo -e "        User $Pengguna has been deleted successfully."
 else
-        echo -e "Error: User $Pengguna is not on the User list."
+        echo -e "        Error: User $Pengguna is not on the User list."
 fi
